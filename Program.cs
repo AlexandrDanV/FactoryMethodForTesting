0f0cf8d84dc38ddaf8406ec9e70037449ae73b52
Sasha_D. <daniav1987@gmail.com>
2024-12-17 14:26:47 +0300

    dop information

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FactoryMethod
 {    
-    /// Comment first: abracadabra
+    /// Comment first: abracadabra  
+    /// This change from you
     class Program
     {
         static void Main(string[] args)
