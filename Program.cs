4e47dc55853ddf152ef87a6eb25df37963b151aa
SashaD <daniav1987@dmail.com>
2024-12-17 14:36:27 +0300

    steep thrid##

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace FactoryMethod
 {    
     /// Comment first: abracadabra  
     /// This change from you
+    ///step Third jfvgfrhgvojrjvgr'jejvrepjvjr'j 
     class Program
     {
         static void Main(string[] args)
